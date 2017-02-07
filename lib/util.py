0c586ae9ea55db8b27e75b8586fbc0a879653ef5
Skirmant <Skirmant@gmx.com>
2017-02-07 18:06:33 +0000

    data folder

--- lib/util.py
+++ lib/util.py
@@ -246,11 +246,11 @@ def user_dir():
     if 'ANDROID_DATA' in os.environ:
         return android_check_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-trump")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-trump")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-trump")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
